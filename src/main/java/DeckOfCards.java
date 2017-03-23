823602f36364ce20698a55f198d2b780b7ea256a
Santosh Bidve <santoshbidave2@gmail.com>
2017-03-23 03:40:52 -0700

    javadoc generated

--- src/main/java/DeckOfCards.java
+++ src/main/java/DeckOfCards.java
@@ -6,8 +6,8 @@ import java.util.Random;
  * Created by santosh on 3/23/2017.
  * Deck of Cards
        52 cards with:
-           ● 4 suits: hearts​, spades​, clubs​, diamonds
-           ● Face values of: Ace​, 2-10​, Jack​, Queen​, and King​).
+           4 suits: hearts​, spades​, clubs​, diamonds
+           Face values of: Ace​, 2-10​, Jack​, Queen​, and King​).
        Within one of your classes, you must provide two operations:
            shuffle()​ - shuffle returns no value, but results in the cards in the deck being randomly permuted
            dealOneCard()​ - returns one card from the deck to the caller.
